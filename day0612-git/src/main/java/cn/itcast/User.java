2b55376b53305942bc2f50d1bb3cf4c890625ea9
wanggqing <wanggqing@126.com>
2019-06-12 16:57:13 +0800

    commit user

---
+++ day0612-git/src/main/java/cn/itcast/User.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String address;
+
+    public static void main(String[] args) {
+        System.out.println("Git...");
+    }
+}
