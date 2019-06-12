48a9df635a343f8ece88aa79537ba71833955695
wanggqing <wanggqing@126.com>
2019-06-12 20:23:25 +0800

    commit user

--- day0612-git/src/main/java/cn/itcast/User.java
+++ day0612-git/src/main/java/cn/itcast/User.java
@@ -5,6 +5,30 @@ public class User {
     private Integer age;
     private String address;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public static void main(String[] args) {
         System.out.println("Git...");
     }
